e04a97525b5b5013ed3f2ac8bcf4bc6d6d748025
John <jhallman5@gmail.com>
2017-07-24 16:01:48 -0700

    Adds RESTful routes

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -1,44 +1,11 @@
 const router = require('express').Router()
-const Albums = require('../../models/queries/albums')
-const User = require('../../models/queries/users')
+const no_auth = require('./no_auth').router
+const albums = require('./albums').router
+const users = require('./users').router
 
-router.get('/', (request, response) => {
-  Albums.getAlbums()
-    .then( albums => response.render('home', { albums: albums }))
-    .catch( error => response.status(500).render('error', { error: error }))
-})
-
-router.get('/albums/:albumID', (request, response) =>
-  Albums.getAlbumsByID(request.params.albumID)
-    .then( albums =>
-      response.render('album', { album: albums[0]})
-    )
-    .catch(error =>
-      response.status(500).render('error', { error: error })
-    )
-)
-
-router.get('/sign_in', (request, response) =>
-  response.render('sign_in')
-)
-
-
-router.get('/sign_up', (request, response) =>
-  response.render('sign_up')
-)
-
-router.post('/sign_up', (request, response) => {
-  const user = request.body
-  User.createUser(user)
-    .then( userId => response.redirect(`/user/${userId[0].id}`) )
-    .catch( error => response.status(500).render('error', { error: error }))
-})
-
-router.get('/user/:userId', (request, response) => {
-  User.getUserById(request.params.userId)
-    .then( data => response.render('user_profile', { data: data[0] }))
-    .catch( error => response.status(500).render('error', { error: error }))
-})
+router.use(no_auth)
+router.use('/user', users)
+router.use('/albums', albums)
 
 router.use( (request, response) =>
   response.status(404).render('not_found')
