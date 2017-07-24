f7b93e5490884dbcaa7f195b0c2feef9f0e4a743
John <jhallman5@gmail.com>
2017-07-24 12:19:04 -0700

    adds user table and seed data

---
+++ models/schema/seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+  albums (title, artist)
+VALUES
+  ('Malibu', 'Anderson .Paak'),
+  ('A Seat at the Table', 'Solange Knowles'),
+  ('Melodrama', 'Lorde'),
+  ('In Rainbows', 'Radiohead')
+;
+
+INSERT INTO
+  users (email, username, password)
+VALUES
+  ('jhallman5@gmail.com', 'jhallman5', 'tomato'),
+  ('steven3@yahoo.com', 'steven4', 'orange'),
+  ('lisa@hotmail.com', 'lisa3', 'banana')
+;
