ad00922d3dbed7b56908371e199372b8e0b0113d
John <jhallman5@gmail.com>
2017-07-25 14:30:30 -0700

    Adds functionality to profile button, renders nav conditionals based on request.session

--- controllers/routes/albums.js
+++ controllers/routes/albums.js
@@ -3,7 +3,7 @@ const Albums = require('../../models/queries/albums')
 
 router.get('/:albumID', (request, response,next) =>
   Albums.getAlbumsByID(request.params.albumID)
-    .then( albums => response.render('album', { album: albums[0]}))
+    .then( albums => response.render('album', { album: albums[0], session: request.session.passport  }))
     .catch( error => response.status(500).render('error', { error: error }))
 )
 
--- controllers/routes/users.js
+++ controllers/routes/users.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const User = require('../../models/queries/users')
 
+router.get('/profile', (request, response, next) => {
+  const { user } = request.session.passport
+  response.redirect(`/user/${user}`)
+})
+
 router.get('/:userId', (request, response, next) => {
-  console.log( "=-=-=-> request.session", request.session.passport.user )
   User.getUserById(request.params.userId)
-    .then( data => response.render('user_profile', { data: data[0] }))
+    .then( data => response.render('user_profile', { data: data[0], session: request.session.passport }))
     .catch( error => response.status(500).render('error', { error: error }))
 })
 
