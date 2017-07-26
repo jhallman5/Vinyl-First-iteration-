68d75cad58605b2d58fc522fe24ec919d6a42905
John <jhallman5@gmail.com>
2017-07-26 10:29:21 -0700

    creates review_card partial

--- models/queries/reviews.js
+++ models/queries/reviews.js
@@ -1,7 +1,7 @@
 const { query } = require('../config_db')
 
 const getReviewsByAlbumId = (albumId) =>
-  query('SELECT * FROM albums a LEFT JOIN reviews r ON a.id = r.album_id WHERE a.id = $1 ORDER BY created_on DESC', [albumId])
+  query('SELECT * FROM albums a LEFT JOIN reviews r ON a.id = r.album_id LEFT JOIN users u ON  r.user_id = u.id WHERE a.id = $1 ORDER BY created_on DESC', [albumId])
 
 const createReview = (albumId, userId, content) =>
   query('INSERT INTO reviews VALUES (DEFAULT, $1, $2, $3)', [albumId, userId, content])
